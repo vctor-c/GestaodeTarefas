dfc9f71dbb0ff8a0f897bf484031f25a65d38bd9
Victor <victor.caetano2008@hotmail.com>
2023-03-26 23:25:35 -0300

    melhorias no codigo

--- js/script.js
+++ js/script.js
@@ -4,42 +4,40 @@ const exibirLista = document.querySelector("#exibirLista")
 const lista = document.querySelector("#lista")
 
 function mostrarLista() {
-    if (tarefa.value.length != 0) { // verifica se existe algum valor no entrada de texto
 
-        // deixa a div visivel
-        exibirLista.classList.remove("d-none")
+    if (exibirLista.classList.contains("d-none")) { // verifica se a div esta visivel
 
-        // cria um novo item
-        const novoItem = document.createElement("li");
-
-        // adiciona a classe para o novo item da lista
-        novoItem.classList.add("list-group-item")
-
-        // cria um conteudo com base na entrada do usuario
-        const conteudo = document.createTextNode(tarefa.value);
-
-        // adiciona o conteudo para a o novo item da lista
-        novoItem.appendChild(conteudo);
-
-        // adiciona o novo item no DOM na posicao 0 da lista
-        lista.insertBefore(novoItem, lista.children[0]);
-
+        exibirLista.classList.remove("d-none") // deixa a div visivel
+        
     }
+
+    const novoItem = document.createElement("li") // cria um novo item
+
+    novoItem.classList.add("list-group-item") // adiciona a classe para o novo item da lista
+
+    const conteudo = document.createTextNode(tarefa.value) // cria um conteudo com base na entrada do usuario
+
+    novoItem.appendChild(conteudo) // adiciona o conteudo para a o novo item da lista
+
+    lista.insertBefore(novoItem, lista.children[0]) // adiciona o novo item no DOM na posicao 0 da lista
+
 }
 
-// chama a funcao mostrarLista ao clicar no botao
-botao.addEventListener("click", mostrarLista)
+botao.addEventListener("click", function () { // espera o clique do usuario
+    if (tarefa.value.length != 0) { // verifica se existe algum valor na entrada de texto
+        mostrarLista() // chama a funcao mostrarLista
+    }
+})
 
-// verifica qual tecla esta sendo precionada
-tarefa.addEventListener("keypress", function (t) {
-
-    if (t.code == "NumpadEnter" || t.code == "Enter") { // se a tecla precionada for enter chama a funcao mostrarLista
-        mostrarLista()
+tarefa.addEventListener("keypress", function (t) { // verifica qual tecla esta sendo pressionada
+    if (tarefa.value.length != 0) { // verifica se existe algum valor na entrada de texto
+        if (t.code == "NumpadEnter" || t.code == "Enter") { // se a tecla pressionada for enter chama a funcao mostrarLista
+            mostrarLista() // chama a funcao mostrarLista
+        }
     }
 });
 
-// pega posicacao quando clicar em algum item da lista
-lista.onclick = function () {
-    event.target.classList.toggle("active") // deixa ativo caso o campo nao estaja ativo, e deixa desativado caso esteja ativado
+lista.onclick = function (e) { // pega posicao quando clicar em algum item da lista
+    e.target.classList.toggle("active") // deixa ativo caso o campo nao estaja ativo, e deixa desativado caso esteja ativado
 }
 
